df9cd98820c42e3ebfd705754602140b9751d1cf
Abdallah <dev.aaamra@gmail.com>
2021-10-02 19:50:31 +0300

    set up database and insert fake data

---
+++ server/database/builds/init.sql
@@ -0,0 +1,36 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users, products, cart CASCADE;
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password text NOT NULL,
+    isAdmin BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE products(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(5,2) NOT NULL,
+    discount DECIMAL(5,2),
+    image text,
+    rate INTEGER DEFAULT 0
+);
+
+
+CREATE TABLE cart(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
+    qunatity INTEGER
+);
+
+insert into users(name, email, password, isAdmin) values ('Nizar', 'nizar@gmail.com', '$2a$10$Bp9etHWYo1UAANX99Ju3LuEzTlH7d1mq8snUpUzV7UhfBlQwozIly', true);
+insert into users(name, email, password, isAdmin) values ('Reem', 'reem@gmail.com', '$2a$10$Bp9etHWYo1UAANX99Ju3LuEzTlH7d1mq8snUpUzV7UhfBlQwozIly', true);
+insert into users(name, email, password, isAdmin) values ('Muath', 'muath@gmail.com', '$2a$10$Bp9etHWYo1UAANX99Ju3LuEzTlH7d1mq8snUpUzV7UhfBlQwozIly', true);
+insert into users(name, email, password, isAdmin) values ('Eman', 'eman@gmail.com', '$2a$10$Bp9etHWYo1UAANX99Ju3LuEzTlH7d1mq8snUpUzV7UhfBlQwozIly', true);
+insert into users(name, email, password, isAdmin) values ('Abdallah', 'abdallah@gmail.com', '$2a$10$Bp9etHWYo1UAANX99Ju3LuEzTlH7d1mq8snUpUzV7UhfBlQwozIly', true);
+
+COMMIT;
