2f7db9f9b3190dfa62e2fc4a66860daffdc058b1
Nizar7zak <nizar7zakout@gmail.com>
2021-10-04 19:02:56 +0300

    handle error server

--- server/controllers/getProductsController.js
+++ server/controllers/getProductsController.js
@@ -1,7 +1,11 @@
 const { getProductsQuery } = require('../database/queries');
 
 const getAllProducts = (req, res) => {
-  getProductsQuery().then((data) => res.json(data));
+  getProductsQuery()
+    .then((data) => res.json(data.rows))
+    .catch(() => {
+      res.status(500).json({ status: 500, msg: 'Server Error' });
+    });
 };
 
 module.exports = getAllProducts;
