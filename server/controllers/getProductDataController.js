a43b7fac12cf1912199d993f3303eb06c0e63a16
Nizar7zak <nizar7zakout@gmail.com>
2021-10-04 19:00:21 +0300

    edit json data

--- server/controllers/getProductDataController.js
+++ server/controllers/getProductDataController.js
@@ -1,7 +1,7 @@
 const { productDataQuery } = require('../database/queries');
 
 const getProductData = (req, res) => {
-  productDataQuery(req.params.id).then((data) => res.json(data));
+  productDataQuery(req.params.id).then((data) => res.json(data.rows));
 };
 
 module.exports = getProductData;
