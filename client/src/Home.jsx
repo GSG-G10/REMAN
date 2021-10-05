9b00c90a26810ae97cba2808db48d2652fbaac0e
Nizar7zak <nizar7zakout@gmail.com>
2021-10-06 02:18:46 +0300

    remove fragment

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,12 +1,11 @@
-import { Fragment } from 'react';
 import BestSellerSection from './components/BestSellerSection';
 import Header from './components/Header';
 
 const Home = () => (
-  <Fragment>
+  <>
     <Header />
     <BestSellerSection />
-  </Fragment>
+  </>
 );
 
 export default Home;
