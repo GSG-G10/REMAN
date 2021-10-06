c8aa8a5a3b784b3977da7172f089dccf57bf5b23
reemsh2000 <sh951652@gmail.com>
2021-10-06 19:50:56 +0300

    add sign node modules

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -1,36 +1,35 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+const { sign } = require('jsonwebtoken');
 // set in cookie use jwt
 
-const {checkEmail, addUser} = require('../database/queries');
-const {regSchema} = require('../utils/validations')
+const { checkEmail, addUser } = require('../database/queries');
+const { regSchema } = require('../utils/validations');
 
 const signup = (req, res) => {
-    const user = req.body;
+  const user = req.body;
 
-    const userValid = regSchema.validateAsync(user);
+  const userValid = regSchema.validateAsync(user);
 
-    const userExist = (rows) => {
-        if(rows.rowCount > 0){
-            throw new Error('user registered');
-        }
+  const userExist = (rows) => {
+    if (rows.rowCount > 0) {
+      throw new Error('user registered');
+    }
+  };
 
-    };
-
-    userValid
-    .then((user) => checkEmail(user.email))
-    .then({ rowCount } => userExist(rowCount))
+  userValid
+    .then((selectedUser) => checkEmail(selectedUser.email))
+    .then(({ rowCount }) => userExist(rowCount))
     .then(() => bcrypt.hash(user.password, 10))
     .then((newPass) => addUser(user, newPass))
-    .then(({rows})=> {
-        const token = jwt.sign({data: rows[0], is_user: true, is_admin: false}, process.env.SECRET_TOKEN);
+    .then(({ rows }) => {
+      // eslint-disable-next-line max-len
+      const token = sign({ data: rows[0], is_user: true, is_admin: false }, process.env.SECRET_TOKEN);
 
-        res.cookie(process.env.COOKIE_AUTH, token, {httponly: true, secure: true}).redirect('/profile');
-  
+      res.cookie(process.env.COOKIE_AUTH, token, { httponly: true, secure: true }).redirect('/profile');
     })
-    .catch(err => {
-        res.status(500).json({'err':err.message})
+    .catch((err) => {
+      res.status(500).json({ err: err.message });
     });
-
 };
 
 module.exports = signup;
