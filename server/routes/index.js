2f681809a6ee51c13d230d87e0a49afdf2e20c54
muath-khatib <mo3az.khatib@hotmail.com>
2021-10-06 22:14:59 +0300

    remove firebase configuration

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,6 @@ const { getProductByCId } = require('../controllers/getProductsByCId');
 const { getProducts, getProductData } = require('../controllers/products');
 
 router.post('/register', signup);
-router.post('/add-new-product', (req, res) => console.log(req.body));
 
 router.get('categories/:categoryId/products', getProductByCId);
 router.get('/', (req, res) => {
@@ -22,11 +21,3 @@ router.get('/categories', getCategories);
 router.delete('/cart/:productId', deleteCartProduct);
 
 module.exports = router;
-
-/* {
-  name: '123',
-  category: 3,
-  price: 12,
-  discount: 33,
-  url: 'https://images.pexels.com/photos/556666/pexels-photo-556666.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-} */
