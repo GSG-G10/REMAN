537199b2dc1578270ef6a12e2222feb7076273c3
reemsh2000 <sh951652@gmail.com>
2021-10-06 16:33:51 +0300

    add login route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const { getProducts, getProductData } = require('../controllers/products');
+const { login, createSession } = require('../controllers/registration');
 
+router.post('/login', login, createSession);
 router.get('/products', getProducts);
 router.get('/products/:id', getProductData);
 
