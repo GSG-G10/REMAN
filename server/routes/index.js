776d59e39015182e775033701862d807dd1e2b0b
reemsh2000 <sh951652@gmail.com>
2021-10-06 21:21:37 +0300

    solve confflict

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,22 +1,17 @@
 const router = require('express').Router();
 
-<<<<<<< HEAD
-=======
 const { getProducts, getProductData, addToCart } = require('../controllers/products');
 const { checkAuth } = require('../middlewares');
 
->>>>>>> ae82fec0c23f60fbefc8aed67b898237f28bfcf6
 const { deleteCartProduct } = require('../controllers/cart');
 const { getCategories } = require('../controllers/categories');
 const { signup } = require('../controllers');
 const { getProductByCId } = require('../controllers/getProductsByCId');
-const { getProducts, getProductData } = require('../controllers/products');
 const { login, createSession } = require('../controllers/registration');
 
 router.post('/login', login, createSession);
 
 router.post('/register', signup);
-
 
 router.get('categories/:categoryId/products', getProductByCId);
 router.get('/', (req, res) => {
