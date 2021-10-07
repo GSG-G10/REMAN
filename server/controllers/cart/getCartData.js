bb6d95cf70b000a527fc9f9f8a544347ef5e5aef
reemsh2000 <sh951652@gmail.com>
2021-10-07 16:35:17 +0300

    ceate cards query

--- server/controllers/cart/getCartData.js
+++ server/controllers/cart/getCartData.js
@@ -1,6 +1,8 @@
 const getCartProducts = require('../../database/queries/cart');
 
 const getCartData = (req, res) => {
+  const { userId } = req;
+  console.log(userId);
   getCartProducts(userId)
     .then((data) => res.json(data))
     .catch(() => res.status(500).json({ status: 500, msg: 'Server Error' }));
