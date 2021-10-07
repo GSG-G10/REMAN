1562cc46e19a9f46d6a74dc190eb1ada2e44c444
reemsh2000 <sh951652@gmail.com>
2021-10-07 16:38:04 +0300

    add cookie parser

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ require('env2')('.env');
 const express = require('express');
 
 const compression = require('compression');
+const cookieParser = require('cookie-parser');
 
 const router = require('./routes');
 
@@ -12,6 +13,7 @@ app.set('port', process.env.PORT || 5000);
 app.disable('x-powered-by');
 app.use(compression());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(router);
 
