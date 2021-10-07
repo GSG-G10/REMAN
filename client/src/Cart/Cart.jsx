9dfe763c276354cbdc0f46cb261a78b19d0af66a
reemsh2000 <sh951652@gmail.com>
2021-10-07 16:34:42 +0300

    cart page'

--- client/src/Cart/Cart.jsx
+++ client/src/Cart/Cart.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header/index';
 import MainCart from './mainCart';
 import Footer from '../components/Footer/Footer';
-
+import axios from 'axios';
+import { Redirect } from 'react-router';
+let history = useHistory();
 
 function Cart() {
+  useEffect(() => {
+    axios
+      .get('/cart')
+      .then()
+      .catch((err) => {
+        if (err.response.status == 401) {
+          history.push('/login');
+        }
+      });
+    return () => {};
+  }, []);
   return (
     <div>
-      <Header/>
-      <MainCart/>
-      <Footer/>
+      <Header />
+      <MainCart />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
