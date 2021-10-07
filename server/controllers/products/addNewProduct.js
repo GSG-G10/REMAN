15f3983c7527ffc4bab6b49e4e5de24a6f1c2ddc
muath-khatib <mo3az.khatib@hotmail.com>
2021-10-07 15:10:55 +0300

    handle error message

--- server/controllers/products/addNewProduct.js
+++ server/controllers/products/addNewProduct.js
@@ -5,7 +5,7 @@ const addNewProduct = (req, res) => {
     name, price, discount, url, category,
   } = req.body;
   addNewProductQuery(name, price, discount, url, category)
-    .then(res.json({ message: 'Product added successfully'}))
+    .then(() => res.json({ message: 'Product added successfully' }))
     .catch(() => res.status(500).json({ message: 'Server Error !' }));
 };
 module.exports = addNewProduct;
