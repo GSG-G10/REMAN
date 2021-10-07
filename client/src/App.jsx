408e09df11100d9aecec5d81cb54e50f5de79a04
Abdallah <dev.aaamra@gmail.com>
2021-10-07 15:13:38 +0300

    front end design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { SignUp } from './components';
 import Login from './components/Login';
 import Home from './pages/Home';
+import Product from './pages/Product';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/home" component={Home} />
+        <Route exact path="/products/:productId" component={Product} />
         <Route path="/" component={Home} />
       </Switch>
     </Router>
